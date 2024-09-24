e9e452178f3f6a36ab799fc07d6f9b7a829a10f3
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-09-25 00:01:17 +0530

    about page responsive

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -11,14 +11,16 @@ export default function About() {
         alt="background-image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover opacity-25"
       />
-      <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0">
+      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0">
         <RenderModel>
           <HatModel />
         </RenderModel>
       </div>
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute flex flex-col items-center text-center top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2 w-full">
-          <h1 className="font-bold text-7xl text-accent">Hrithik Varshney</h1>
+        <div className="absolute flex flex-col items-center text-center top-[50%] sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2 w-full">
+          <h1 className="font-bold text-4xl xs:text-5xl sm:text-6xl lg:text-7xl text-accent">
+            Hrithik Varshney
+          </h1>
           <p className="font-light text-foreground text-lg">
             Hi! I am a Software Developer
           </p>
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -5,7 +5,7 @@ const ItemLayout = ({ children, className }) => {
   return (
     <div
       className={clsx(
-        "custom-bg p-8 rounded-xl flex items-center justify-center space-y-6",
+        "custom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-6",
         className
       )}
     >
@@ -17,12 +17,12 @@ const ItemLayout = ({ children, className }) => {
 const AboutDetails = () => {
   return (
     <section className="py-20 w-full">
-      <div className="grid grid-cols-12 gap-8 w-full ">
-        <ItemLayout className="col-span-8 row-span-2 flex-col items-start">
-          <h2 className="text-2xl text-left w-full capitalize">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full ">
+        <ItemLayout className="col-span-full lg:col-span-8 row-span-2 flex-col items-start">
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
             Software Developer
           </h2>
-          <p className="font-light">
+          <p className="font-light text-xs sm:text-sm md:text-base">
             2+ Year of experience in Software Development with proficiency in
             designing and developing enterprise-based applications. Excellent
             technical experience on ReactJS, JavaScript, TypeScript, HTML5,
@@ -31,22 +31,22 @@ const AboutDetails = () => {
             Unit Test cases.
           </p>
         </ItemLayout>
-        <ItemLayout className="col-span-4 text-accent">
-          <p className="font-semibold w-full text-left text-5xl">
+        <ItemLayout className="col-span-full xs:col-span-6 lg:col-span-4 text-accent">
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
             7{" "}
             <sub className="font-semibold text-base">
               Projects Delivered with Current Company
             </sub>
           </p>
         </ItemLayout>
-        <ItemLayout className="col-span-4 text-accent">
-          <p className="font-semibold w-full text-left text-5xl">
+        <ItemLayout className="col-span-full xs:col-span-6 lg:col-span-4 text-accent">
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
             2+{" "}
             <sub className="font-semibold text-base">Years of Experience</sub>
           </p>
         </ItemLayout>
 
-        <ItemLayout className="col-span-4 !p-0">
+        <ItemLayout className="col-span-full sm:col-span-6 md:col-span-4 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-stats.vercel.app/api/top-langs/?username=Hrithik08-Varshney&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FEFE5B&text_bold=false"
@@ -55,7 +55,7 @@ const AboutDetails = () => {
           />
         </ItemLayout>
 
-        <ItemLayout className="col-span-8 !p-0">
+        <ItemLayout className="col-span-full md:col-span-8 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-stats.vercel.app/api?username=Hrithik08-Varshney&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FEFE5B&text_bold=false"
@@ -72,7 +72,7 @@ const AboutDetails = () => {
             loading="lazy"
           />
         </ItemLayout>
-        <ItemLayout className="col-span-6 !p-0">
+        <ItemLayout className="col-span-full md:col-span-6 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-streak-stats.herokuapp.com?user=Hrithik08-Varshney&theme=dark&hide_border=true"
@@ -81,7 +81,7 @@ const AboutDetails = () => {
           />
         </ItemLayout>
 
-        <ItemLayout className="col-span-6 !p-0">
+        <ItemLayout className="col-span-full md:col-span-6 !p-0">
           <img
             className="w-full h-auto"
             src="https://github-readme-stats.vercel.app/api/pin/?username=Hrithik08-Varshney&repo=MOVIX&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FEFE5B&text_bold=false"
@@ -95,4 +95,3 @@ const AboutDetails = () => {
 };
 
 export default AboutDetails;
-
