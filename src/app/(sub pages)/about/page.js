2df21d58b5d414aec70038ecd54630630c7face6
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-09-28 11:31:35 +0530

    image optimisation

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
 import RenderModel from "@/components/RenderModel";
-import HatModel from "@/components/models/HatModel";
 import AboutDetails from "@/components/about";
+import dynamic from "next/dynamic";
+
+const HatModel = dynamic(() => import("@/components/models/HatModel"), {
+  ssr: false,
+});
 export default function About() {
   return (
     <>
       <Image
+        priority
+        sizes="100vw"
         src={bg}
         alt="background-image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover opacity-25"
