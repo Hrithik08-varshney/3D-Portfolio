81ca670709b595ea43abca785284bd5d6b8ae9ff
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-10-11 16:45:02 +0530

    description added

--- src/app/data.js
+++ src/app/data.js
@@ -20,42 +20,47 @@ export const projectsData = [
   {
     id: 1,
     name: "Movix",
-    urlName:"Movix",
+    urlName: "Movix",
     description: "Get All Movies Information",
     technology: ["React", "Js", "Vite", "Redux"],
     gitLink: "https://github.com/Hrithik08-varshney/MOVIX",
+    desc: "Movix is your ultimate movie companion, developed in React to deliver a seamless and engaging experience for film enthusiasts. With an intuitive interface, users can easily browse and search a vast library of movies, accessing detailed information such as synopses, cast and crew details, release dates, ratings, and genres. The app offers personalized recommendations tailored to your viewing preferences, along with user reviews and ratings to help you make informed choices. You can create and manage a watchlist to keep track of films you want to see, all while enjoying a responsive design that works beautifully across devices. Stay updated on the latest releases and discover hidden gems with Movix, making your movie-watching experience truly unforgettable!",
   },
   {
     id: 2,
     name: "React Weather App",
-    urlName:"React-Weather-App",
+    urlName: "React-Weather-App",
     description: "Check the weather of your city",
     technology: ["React", "Js"],
     gitLink: "https://github.com/Hrithik08-varshney/React-Weather-App",
+    desc:"WeatherApp is a dynamic and user-friendly application built with React that brings the latest weather updates right to your fingertips. By integrating with the OpenWeather API, the app provides real-time weather data for locations around the globe. With a clean and intuitive interface, users can easily search for cities to access current conditions, forecasts, humidity levels, wind speeds, and more."
   },
   {
     id: 3,
     name: "Aora",
-    urlName:"Aora",
+    urlName: "Aora",
     description: "Tiktok similar Mobile App",
     technology: ["ReactNative", "Js", "Appwrite", "Expo", "Tailwind"],
     gitLink: "https://github.com/Hrithik08-varshney/React-Native-Learn-App",
+    desc:"Aora is a vibrant mobile application developed using React Native and powered by Expo, designed to create a dynamic and engaging social video-sharing experience similar to TikTok. With Aora, users can effortlessly create, edit, and share short videos, showcasing their creativity and connecting with a global community. To ensure seamless performance and user management, Aora’s backend is powered by Appwrite, providing robust features for authentication, database management, and real-time interactions."
   },
   {
     id: 4,
     name: "Chat App",
-    urlName:"Chat-App",
+    urlName: "Chat-App",
     description: "Built a group chat here",
     technology: ["NodeJs", "HTML", "CSS", "Js", "Socket"],
     gitLink: "https://github.com/Hrithik08-varshney/Chat-App",
+    desc:"ChatApp is a sleek and responsive chat application designed for real-time communication, with a front end crafted using HTML, CSS, and JavaScript. The user-friendly interface allows users to easily send and receive messages, making it perfect for both casual conversations and group chats. On the back end, ChatApp leverages Node.js along with Socket.io to enable seamless real-time messaging. This powerful combination ensures instant message delivery and updates, keeping users connected without any lag. The app features user authentication, allowing users to create accounts and manage their profiles while enjoying a secure chatting environment."
   },
   {
     id: 5,
     name: "FakeShop",
-    urlName:"FakeShop",
+    urlName: "FakeShop",
     description: "Shopping App Clone",
     technology: ["React", "Js", "Vite", "Redux"],
     gitLink: "https://github.com/Hrithik08-varshney/fakeshop",
+    desc:"Shopping Clone is a feature-rich e-commerce application developed with React and powered by Redux for state management, designed to provide a seamless online shopping experience. This app mimics popular shopping platforms, allowing users to browse a wide array of products, add items to their cart, and make secure purchases with ease."
   },
   {
     id: 6,
@@ -64,6 +69,7 @@ export const projectsData = [
     description: "Read your daily news here",
     technology: ["React", "Js", "Redux"],
     gitLink: "https://github.com/Hrithik08-varshney/Inshort-Clone",
+    desc:"Short App Clone is a sleek application developed with React that showcases the power of API integration and infinite scrolling. This app allows users to seamlessly browse through a vast collection of content without interruptions, providing an engaging user experience. With smooth navigation and a clean interface, users can effortlessly explore items, while the infinite scrolling feature ensures that new content loads automatically as they reach the bottom of the page. Perfect for discovering endless information, Short App Clone delivers a modern and efficient browsing experience!"
   },
   {
     id: 7,
@@ -72,6 +78,7 @@ export const projectsData = [
     description: "Add your daily task here",
     technology: ["React", "Firebase"],
     gitLink: "https://github.com/Hrithik08-varshney/Todo-List",
+    desc:"The ToDo List App is a simple yet powerful productivity tool developed using React, with a robust backend powered by Firebase. This app allows users to easily create, manage, and track their tasks in an intuitive interface. With real-time synchronization provided by Firebase, users can access their to-do lists from any device, ensuring that updates are instantly reflected across all platforms. The app features functionalities like adding, editing, and deleting tasks, as well as marking items as complete. Users can also categorize tasks and set deadlines to stay organized."
   },
   {
     id: 8,
@@ -80,6 +87,7 @@ export const projectsData = [
     description: "Admin portal for your business",
     technology: ["React", "SCSS"],
     gitLink: "https://github.com/Hrithik08-varshney/React-Admin-Dashboard",
+    desc:"The Admin Dashboard is a powerful and customizable application developed using React and styled with SCSS, designed to provide an intuitive interface for managing various administrative tasks. With a modern and responsive design, this dashboard offers a seamless user experience across devices. Featuring a variety of pre-built components such as charts, tables, and forms, the dashboard allows administrators to easily monitor key metrics, manage users, and access detailed analytics. The use of SCSS ensures that the design is both flexible and maintainable, allowing for quick adjustments and enhancements."
   },
   {
     id: 9,
@@ -129,22 +137,22 @@ export const projectsData = [
     technology: ["HTML", "CSS", "Js", "SVG"],
     gitLink: "https://github.com/Hrithik08-varshney/StopWatch",
   },
-  // {
-  //   id: 15,
-  //   name: "Review Carousel",
-  //   urlName: "Review-Carousel",
-  //   description: "Get latest reviews here",
-  //   technology: ["HTML", "CSS", "Js"],
-  //   gitLink: "https://github.com/Hrithik08-varshney/Review-Carousel",
-  // },
-  // {
-  //   id: 16,
-  //   name: "Websites",
-  //   urlName: "Websites",
-  //   description: "Get Different types of website here",
-  //   technology: ["HTML", "CSS", "Js"],
-  //   gitLink: "https://github.com/Hrithik08-varshney/Different-Websites",
-  // },
+  {
+    id: 15,
+    name: "Review Carousel",
+    urlName: "Review-Carousel",
+    description: "Get latest reviews here",
+    technology: ["HTML", "CSS", "Js"],
+    gitLink: "https://github.com/Hrithik08-varshney/Review-Carousel",
+  },
+  {
+    id: 16,
+    name: "Websites",
+    urlName: "Websites",
+    description: "Get Different types of website here",
+    technology: ["HTML", "CSS", "Js"],
+    gitLink: "https://github.com/Hrithik08-varshney/Different-Websites",
+  },
 ];
 
 export const BtnList = [
