287d543adb85ea0e5007b67bccb1719532fcb95a
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-10-15 23:39:40 +0530

    image names fixed

--- src/app/(sub pages)/projects/projectInfo/[projectName]/page.js
+++ src/app/(sub pages)/projects/projectInfo/[projectName]/page.js
@@ -1,5 +1,7 @@
 "use client";
 import { getCurrentProjectData } from "@/common";
+import { ItemLayout } from "@/components/about/ItemLayout";
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 const ProjectInfo = ({ params }) => {
   const { projectName } = params;
@@ -7,7 +9,13 @@ const ProjectInfo = ({ params }) => {
   useEffect(() => {
     setProjectData(getCurrentProjectData(projectName));
   }, [projectName]);
-  return <div>{projectData?.name}</div>;
+  return (
+    <div>
+      <ItemLayout className="col-span-full lg:col-span-8 row-span-2 flex-col items-start">
+      {/* <Image src={} alt=""/> */}
+      </ItemLayout>
+    </div>
+  );
 };
 
 export default ProjectInfo;
