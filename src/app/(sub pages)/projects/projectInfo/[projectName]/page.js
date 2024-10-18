272dcffa392f2238ab9c9c17544a15df1eec2864
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-10-18 23:16:54 +0530

    project page changes

--- src/app/(sub pages)/projects/projectInfo/[projectName]/page.js
+++ src/app/(sub pages)/projects/projectInfo/[projectName]/page.js
@@ -3,6 +3,7 @@ import { getCurrentProjectData } from "@/common";
 import { ItemLayout } from "@/components/about/ItemLayout";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import ImageNotFound from "../../../../../assets/ImageNotFound.jpg";
 const ProjectInfo = ({ params }) => {
   const { projectName } = params;
   const [projectData, setProjectData] = useState();
@@ -12,7 +13,11 @@ const ProjectInfo = ({ params }) => {
   return (
     <div>
       <ItemLayout className="col-span-full lg:col-span-8 row-span-2 flex-col items-start">
-      {/* <Image src={} alt=""/> */}
+        {projectData.image ? (
+          <Image src={projectData.image} alt={projectData.name} />
+        ) : (
+          <Image src={ImageNotFound} alt="imageNotFound" />
+        )}
       </ItemLayout>
     </div>
   );
