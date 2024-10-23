3d5e92258e8f735fb8452fccdaecbf75471331cc
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-10-24 00:01:44 +0530

    responsive done

--- src/app/(sub pages)/projects/projectInfo/[projectName]/page.js
+++ src/app/(sub pages)/projects/projectInfo/[projectName]/page.js
@@ -3,7 +3,9 @@ import { getCurrentProjectData } from "@/common";
 import { ItemLayout } from "@/components/about/ItemLayout";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import ImageNotFound from "../../../../../assets/ImageNotFound.jpg";
+import Link from "next/link";
 const ProjectInfo = ({ params }) => {
   const { projectName } = params;
   const [projectData, setProjectData] = useState();
@@ -11,24 +13,48 @@ const ProjectInfo = ({ params }) => {
     setProjectData(getCurrentProjectData(projectName));
   }, [projectName]);
   return (
-    <div className="flex w-[100%]">
-    <div className="w-[50%]">
-
-      <ItemLayout>
-        {projectData?.image ? (
-          <Image src={projectData?.image} alt={projectData.name} />
-        ) : (
-          <Image src={ImageNotFound} alt="imageNotFound" />
-        )}
-      </ItemLayout>
-    </div>
-      <div className=" w-[60%] grid grid-cols-4 grid-rows-2 gap-4 p-6 sm:p-8">
-      <div className=" h-auto flex col-span-1 items-center justify-center custom-bg rounded-xl">GITHUB</div>
-      <div className=" h-auto flex col-span-3 items-center justify-center custom-bg rounded-xl cursor-pointer">{projectData?.gitLink}</div>
-      <div className=" h-auto flex col-span-1 items-center justify-center custom-bg rounded-xl">DEMO</div>
-      <div className=" h-auto flex col-span-3 items-center justify-center custom-bg rounded-xl cursor-pointer">{projectData?.deployLink}</div>
+    <>
+      <div className="flex flex-col sm:flex-col lg:flex-row w-[100%]">
+        <div className="sm:w-[100%] lg:w-[50%]">
+          <ItemLayout>
+            {projectData?.image ? (
+              <Image src={projectData?.image} alt={projectData.name} />
+            ) : (
+              <Image src={ImageNotFound} alt="imageNotFound" />
+            )}
+          </ItemLayout>
+        </div>
+        <motion.div
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
+          className="sm:w-[100%] lg:w-[70%] grid grid-cols-4 grid-rows-2 gap-4 p-6 sm:p-8"
+        >
+          <div className="h-[50px] text-xs sm:text-sm lg:h-auto flex col-span-1 items-center justify-center custom-bg rounded-xl">
+            GITHUB
+          </div>
+          <Link
+            href={projectData?.gitLink || ""}
+            target={"_blank"}
+            className=" h-[50px] text-xs sm:text-sm lg:h-auto flex col-span-3 items-center justify-center custom-bg rounded-xl cursor-pointer"
+          >
+            {projectData?.gitLink}
+          </Link>
+          <div className="h-[50px] text-xs  sm:text-sm lg:h-auto  flex col-span-1 items-center justify-center custom-bg rounded-xl">
+            DEMO
+          </div>
+          <Link
+            href={projectData?.deployLink || ""}
+            target={"_blank"}
+            className=" h-[50px] text-xs sm:text-sm lg:h-auto flex col-span-3 items-center justify-center custom-bg rounded-xl cursor-pointer"
+          >
+            {projectData?.deployLink}
+          </Link>
+        </motion.div>
       </div>
-    </div>
+      <ItemLayout className="mt-5">{projectData?.fullDesc}</ItemLayout>
+    </>
   );
 };
 
